510bc1c38e998a6a50691f670a81a6aaa537ebf8
Lelikbjmak <denissolovey990@gmail.com>
2023-05-06 11:49:52 +0300

    fix: Fix storing CovidDetails for today in Redis instead of simple DB.

--- src/main/java/com/innowise/covidapi/service/impl/MaxMinCovidDetailsEvaluatorServiceImpl.java
+++ src/main/java/com/innowise/covidapi/service/impl/MaxMinCovidDetailsEvaluatorServiceImpl.java
@@ -5,8 +5,8 @@ import com.innowise.covidapi.dto.MaxMinCovidDetailsDto;
 import com.innowise.covidapi.entity.id.CountryCovidDetailsId;
 import com.innowise.covidapi.mapper.CovidDetailsMapper;
 import com.innowise.covidapi.service.CountryService;
-import com.innowise.covidapi.service.MaxMinCovidDetailsEvaluatorService;
 import com.innowise.covidapi.service.CovidDetailsService;
+import com.innowise.covidapi.service.MaxMinCovidDetailsEvaluatorService;
 import com.innowise.covidapi.util.CovidDetailsIdProvider;
 import jakarta.enterprise.context.ApplicationScoped;
 import lombok.RequiredArgsConstructor;
@@ -28,12 +28,14 @@ public class MaxMinCovidDetailsEvaluatorServiceImpl implements MaxMinCovidDetail
 
     private final ApiResponseParserService apiResponseParserService;
 
+    private final CountryCovidDetailsRedisService countryCovidDetailsRedisService;
+
     @Override
     public MaxMinCovidDetailsDto getCovidDetailsForToday(List<String> countryNameList) {
 
         List<CountryCovidDetailsId> requestedCovidDetailsIdList = CovidDetailsIdProvider.getCovidDetailsIdListForCountryListForDate(countryNameList, LocalDate.now());
 
-        List<CountryCovidDetailsDto> presentedInDatabaseCovidDetailsList = covidDetailsService.findByIdList(requestedCovidDetailsIdList);
+        List<CountryCovidDetailsDto> presentedInDatabaseCovidDetailsList = countryCovidDetailsRedisService.findByIdList(requestedCovidDetailsIdList);
 
         if (presentedInDatabaseCovidDetailsList.size() == countryNameList.size())
             return evaluateMaxAndMinCovidDetailsForCovidDetailsList(presentedInDatabaseCovidDetailsList);
@@ -45,7 +47,7 @@ public class MaxMinCovidDetailsEvaluatorServiceImpl implements MaxMinCovidDetail
         List<CountryCovidDetailsDto> notPresentedInDatabaseCovidDetails = covidDetailsService.ascertainCovidDetailsNotPresentedInDatabase(remainCountryNameList, todayApiCovidDetailsDtoList);
 
         notPresentedInDatabaseCovidDetails.forEach(countryCovidDetailsDto ->
-                covidDetailsService.save(covidDetailsMapper.mapToEntity(countryCovidDetailsDto)));
+                countryCovidDetailsRedisService.save(covidDetailsMapper.mapToEntity(countryCovidDetailsDto)));
 
         List<CountryCovidDetailsDto> completeCovidDetailsList = new ArrayList<>();
         completeCovidDetailsList.addAll(notPresentedInDatabaseCovidDetails);
@@ -96,9 +98,6 @@ public class MaxMinCovidDetailsEvaluatorServiceImpl implements MaxMinCovidDetail
 
         List<CountryCovidDetailsDto> getDyaApiCovidDetailsDtoList = apiResponseParserService.getTermCovidDetailsFromApi(remainCountrySlugList, firstDate, lastDate.plusDays(2)); // plus 2 due to API incorrect work
         List<CountryCovidDetailsDto> notPresentedInDatabaseCovidDetails = covidDetailsService.ascertainCovidDetailsNotPresentedInDatabase(remainCountryNameList, getDyaApiCovidDetailsDtoList);
-
-        notPresentedInDatabaseCovidDetails.forEach(countryCovidDetailsDto ->
-                covidDetailsService.save(covidDetailsMapper.mapToEntity(countryCovidDetailsDto)));
 
         List<CountryCovidDetailsDto> completeCovidDetailsList = new ArrayList<>();
         completeCovidDetailsList.addAll(notPresentedInDatabaseCovidDetails);
