79e8b4625cbe2ed6d072f77278c8effc362cbffe
Lelikbjmak <denissolovey990@gmail.com>
2023-05-13 20:46:04 +0300

    feature: Add Message constants.

--- src/main/java/com/innowise/covidapi/util/ApplicationConstant.java
+++ src/main/java/com/innowise/covidapi/util/ApplicationConstant.java
@@ -12,4 +12,8 @@ public class ApplicationConstant {
         public static final long RATE_LIMIT_RESET_HEADER_VALUE = 5000;
     }
 
+    @UtilityClass
+    public static class Message {
+        public static final String NOT_VALID_COUNTRY_MESSAGE = "Some country in List is not found.";
+    }
 }
