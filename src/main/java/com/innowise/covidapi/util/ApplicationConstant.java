d0e64d886d7bb1becd7216bf96acd1700ff8acb9
Lelikbjmak <denissolovey990@gmail.com>
2023-05-14 12:09:28 +0300

    feature: Add first reordered date and error message for date.

--- src/main/java/com/innowise/covidapi/util/ApplicationConstant.java
+++ src/main/java/com/innowise/covidapi/util/ApplicationConstant.java
@@ -2,6 +2,8 @@ package com.innowise.covidapi.util;
 
 import lombok.experimental.UtilityClass;
 
+import java.time.LocalDate;
+
 @UtilityClass
 public class ApplicationConstant {
 
@@ -10,10 +12,14 @@ public class ApplicationConstant {
         public static final String RATE_LIMIT_RESET_HEADER_NAME = "X-Ratelimit-Remaining";
 
         public static final long RATE_LIMIT_RESET_HEADER_VALUE = 5000;
+
+        public static final LocalDate FIRST_RECORDER_DATE = LocalDate.of(2020, 2, 25);
     }
 
     @UtilityClass
     public static class Message {
         public static final String NOT_VALID_COUNTRY_MESSAGE = "Some country in List is not found.";
+
+        public static final String NOT_VALID_DATE_MESSAGE = "Date should be after 2020-02-25 and before current date.";
     }
 }
